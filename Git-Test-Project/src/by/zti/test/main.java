04b81a06fe787b6c02da784a055df816d5ea1c1b
taranenko <tmmpbigmin@gmail.com>
2014-03-25 14:53:59 +0200

    Лесенс 18

--- Git-Test-Project/src/by/zti/test/main.java
+++ Git-Test-Project/src/by/zti/test/main.java
@@ -1,10 +1,22 @@
 package by.zti.test;
 
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
 public class main {
+	private static ArrayList<Profile> profiles = new ArrayList<Profile>();
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub !!!!");
-
-	}
-
+		System.out.println(profiles.size());
+		Profile profile = new Profile();
+		profile.setName(JOptionPane.showInputDialog(null, "¬ведите ваше им€"));
+		profile.setSurname(JOptionPane.showInputDialog(null, "¬ведите вашу фамилию"));
+		profiles.add(profile);
+		for (Profile p : profiles){
+			System.out.println(p.getName()+" "+p.getSurname());
+		}
+		System.out.println(profiles.size());
+	}	
 }
+
